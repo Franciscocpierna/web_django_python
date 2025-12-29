0160bebdab31135bd2cb750543c10a5f8f481a6e
Francisco Carlos Pierna <pierna_f@hotmail.com>
2025-12-29 10:35:04 -0300

    shell  29/12/2025

--- Django1/core/views.py
+++ Django1/core/views.py
@@ -2,9 +2,19 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
+    print(f"user =  {request.user}")
+
+    if str(request.user) == 'AnonymousUser':
+        teste = "usuario não logado"
+        
+    else:
+        teste = "usuario logado"
+           
     context = {
         'curso': 'Programação web com Django Framework',
-        'outros': 'Django é massa ' 
+        'outros': 'Django é massa ',
+        'logado': teste 
+
     }
     return render(request, 'index.html', context)
 
