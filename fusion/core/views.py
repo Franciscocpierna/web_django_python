cc8ab9d02d1cd64b32cbadb9e0419bad474acef2
Francisco Carlos Pierna <pierna_f@hotmail.com>
2026-01-22 08:25:15 -0300

    projeto fusion 22/01/2026

--- fusion/core/views.py
+++ fusion/core/views.py
@@ -7,6 +7,7 @@ from django.views.generic import TemplateView
 #from .models import Servico, Funcionario
 #from .forms import ContatoForm
 
+
 class IndexView(TemplateView):
     template_name = 'index.html'
 
